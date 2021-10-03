34761743cfdced976f6509a08172a33d7d83dff6
Fernando Forastieri <54296311+ferforastieri@users.noreply.github.com>
2021-10-03 23:04:22 +0000

    É possivel adicionar playlists, e pausar o bot agora.

--- cogs/music.py
+++ cogs/music.py
@@ -1,67 +1,109 @@
+import pprint
+import asyncio
+
 import discord
 from discord.ext import commands
 
 from youtube_dl import YoutubeDL
+import re
+
+URL_REG = re.compile(r'https?://(?:www\.)?.+')
+YOUTUBE_VIDEO_REG = re.compile(r"(https?://)?(www\.)?youtube\.(com|nl)/watch\?v=([-\w]+)")
 
 class music(commands.Cog):
     def __init__(self, client):
         self.client = client
-    
+
         #all the music related stuff
         self.is_playing = False
+        self.event = asyncio.Event()
 
         # 2d array containing [song, channel]
         self.music_queue = []
-        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+        self.YDL_OPTIONS = {
+            'format': 'bestaudio/best',
+            'restrictfilenames': True,
+            'noplaylist': True,
+            'nocheckcertificate': True,
+            'ignoreerrors': False,
+            'logtostderr': False,
+            'quiet': True,
+            'no_warnings': True,
+            #'default_search': 'auto',
+            'extract_flat': True
+        }
         self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
         self.vc = ""
 
      #searching the item on youtube
     def search_yt(self, item):
+
         with YoutubeDL(self.YDL_OPTIONS) as ydl:
-            try: 
-                info = ydl.extract_info("ytsearch:%s" % item, download=False)['entries'][0]
+            try:
+                if (yt_url:=YOUTUBE_VIDEO_REG.match(item)):
+                    item = yt_url.group()
+                elif not URL_REG.match(item):
+                    item = f"ytsearch:{item}"
+                info = ydl.extract_info(item, download=False)
             except Exception: 
                 return False
 
-        return {'source': info['formats'][0]['url'], 'title': info['title']}
+        try:
+            entries = info["entries"]
+        except KeyError:
+            entries = [info]
 
-    def play_next(self):
-        if len(self.music_queue) > 0:
-            self.is_playing = True
+        if info["extractor_key"] == "YoutubeSearch":
 
-            #get the first url
-            m_url = self.music_queue[0][0]['source']
+            entries = entries[:1]
 
-            #remove the first element as you are currently playing it
-            self.music_queue.pop(0)
+        tracks = []
 
-            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
-        else:
-            self.is_playing = False
+        for t in entries:
+
+            tracks.append({'source': f'https://www.youtube.com/watch?v={t["id"]}', 'title': t['title']})
+
+        #if search:
+        #    tracks = {'source': info['formats'][0]['url'], 'title': info['title']}
+
+        return tracks
 
     # infinite loop checking 
     async def play_music(self):
+
+        self.event.clear()
+
         if len(self.music_queue) > 0:
+
             self.is_playing = True
 
             m_url = self.music_queue[0][0]['source']
-            
+
+            # If source was a stream (not downloaded), so we should regather to prevent stream expiration
+            with YoutubeDL(self.YDL_OPTIONS) as ydl:
+                try:
+                    info = ydl.extract_info(m_url, download=False)
+                    m_url = info['formats'][0]['url']
+                except Exception:
+                    return False
+
             #try to connect to voice channel if you are not already connected
 
             if self.vc == "" or not self.vc.is_connected() or self.vc == None:
                 self.vc = await self.music_queue[0][1].connect()
             else:
                 await self.vc.move_to(self.music_queue[0][1])
-            
-            print(self.music_queue)
+
             #remove the first element as you are currently playing it
             self.music_queue.pop(0)
 
-            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda l: self.client.loop.call_soon_threadsafe(self.event.set))
+            await self.event.wait()
+            await self.play_music()
         else:
             self.is_playing = False
+            self.music_queue.clear()
             await self.vc.disconnect()
 
     @commands.command(name="help",alisases=['ajuda'],help="Comando de ajuda")
@@ -72,15 +114,14 @@ class music(commands.Cog):
         embedhelp = discord.Embed(
             colour = 1646116,#grey
             title=f'Comandos do {self.client.user.name}',
-            description = helptxt+''
+            description = helptxt+'\n'
         )
         embedhelp.set_thumbnail(url=self.client.user.avatar_url)
         await ctx.send(embed=embedhelp)
 
 
     @commands.command(name="play", help="Toca uma música do YouTube",aliases=['p','tocar'])
-    async def p(self, ctx, *args):
-        query = " ".join(args)
+    async def p(self, ctx: commands.Context, *, query:str = "Rick Astley - Never Gonna Give You Up "):
         
         try:
             voice_channel = ctx.author.voice.channel
@@ -94,21 +135,28 @@ class music(commands.Cog):
             await ctx.send(embed=embedvc)
             return
         else:
-            song = self.search_yt(query)
-            if type(song) == type(True):
+            songs = self.search_yt(query)
+            if type(songs) == type(True):
                 embedvc = discord.Embed(
                     colour= 12255232,#red
                     description = 'Algo deu errado! Tente mudar ou configurar a playlist/vídeo ou escrever o nome dele novamente!'
                 )
                 await ctx.send(embed=embedvc)
             else:
+
+                if (size:=len(songs)) > 1:
+                    txt = f"Você adicionou **{size} músicas** na fila!"
+                else:
+                    txt = f"Você adicionou a música **{songs[0]['title']}** à fila!"
+
                 embedvc = discord.Embed(
                     colour= 32768,#green
-                    description = f"Você adicionou a música **{song['title']}** à fila!"
+                    description = f"{txt}\n\n"
                 )
                 await ctx.send(embed=embedvc)
-                self.music_queue.append([song, voice_channel])
-                
+                for song in songs:
+                    self.music_queue.append([song, voice_channel])
+
                 if self.is_playing == False:
                     await self.play_music()
 
@@ -137,8 +185,6 @@ class music(commands.Cog):
     async def skip(self, ctx):
         if self.vc != "" and self.vc:
             self.vc.stop()
-            #try to play next in the queue if it exists
-            await self.play_music()
             embedvc = discord.Embed(
                 colour= 1646116,#ggrey
                 description = f"Você pulou a música."
@@ -155,6 +201,34 @@ class music(commands.Cog):
             await ctx.send(embed=embedvc)     
         else:
             raise error
+
+    @commands.command(name="stop",help="Para o player de tocar músicas",aliases=["parar", "sair", "leave", "l"])
+    async def stop(self, ctx: commands.Context):
+
+        embedvc = discord.Embed(colour= 12255232)
+
+        if not ctx.me.voice:
+            embedvc.description = "Não estou conectado em um canal de voz."
+            await ctx.reply(embed=embedvc)
+            return
+
+        if not ctx.author.voice or ctx.author.voice.channel != ctx.me.voice.channel:
+            embedvc.description = "Você precisa estar no meu canal de voz atual para usar esse comando."
+            await ctx.reply(embed=embedvc)
+            return
+
+        if any(m for m in ctx.me.voice.channel.members if not m.bot and m.guild_permissions.manage_channels) and not ctx.author.guild_permissions.manage_channels:
+            embedvc.description = "No momento você não tem permissão para usar esse comando."
+            await ctx.reply(embed=embedvc)
+            return
+
+        self.is_playing = False
+        self.music_queue.clear()
+        await self.vc.disconnect(force=True)
+
+        embedvc.colour = 1646116
+        embedvc.description = "Você parou o player"
+        await ctx.reply(embed=embedvc)
 
 def setup(client):
     client.add_cog(music(client))
--- main.py
+++ main.py
@@ -11,8 +11,15 @@ client = commands.Bot(command_prefix = "m!", case_insensitive = True, intents=in
 
 client.remove_command('help')
 
+@client.event
+async def on_ready():
+    print('Entramos como {0.user}'.format(client))
+
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="músicas"))
+
+
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('Seu token')
+client.run('Seu Token')
